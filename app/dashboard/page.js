fb7346f3e87d90015b42a786f513279f9f7ca016
salman165324 <salmansadi165324@gmail.com>
2024-09-09 23:05:26 +0600

    Made the dashboard page better

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import { Card } from "@/components/ui/card";
+import React from "react";
 
 const page = () => {
   return (
-    <div className='m-5'>
-        Dashboard Page
+    <div className="mt-10">
+      <div class="overflow-hidden rounded-lg bg-yellow-100 shadow w-auto mx-5">
+        <div class="px-4 py-5 sm:p-6 text-3xl text-black text-center"> Demo Dashboard</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default page; 
+export default page;
